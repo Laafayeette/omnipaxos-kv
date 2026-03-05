bd46129cda0334a3e84e1480bf6b09e543f3e692
Teoman Köylüoglu <151879574+Laafayeette@users.noreply.github.com>
2026-03-05 23:04:21 +0100

    fix: Add Documentation to Public Declarations (#1)
    Fixed and added documentation to fields and public declarations.
    
    Re-named elapsed_micros to elapsed_real_micros for clarity.

--- src/clock.rs
+++ src/clock.rs
@@ -6,13 +6,14 @@ pub enum ClockSimError {
 }
 
 use std::time::{Duration,Instant, SystemTime, UNIX_EPOCH};
+
+/// A clock simulator that models a synchronized clock with configurable parameters.
 pub struct ClockSimulator {
     drift_rate: f64,            // microseconds per second
     uncertainty: i64,           // microseconds
-    last_sync_system: Instant,// system time
     sync_interval: Duration, // the sync interval
-    last_sync_simulated: i64,   // micro seconds
-
+    last_sync_system: Instant, // Monotonic reference point captured at the last sync (used to measure elapsed time).
+    last_sync_simulated: i64,   // Simulated clock reading at the last sync (µs since UNIX_EPOCH).
 }
 
 impl ClockSimulator {
@@ -29,7 +30,7 @@ impl ClockSimulator {
             return Err(ClockSimError::NegativeDriftRate(drift_rate));
         }
 
-        let now = SystemTime::now()
+        let now = SystemTime::now() // Current C_i(t) reading
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_micros() as i64;
@@ -44,25 +45,30 @@ impl ClockSimulator {
 
     }
 
-
+    /// Returns the current simulated time in microseconds since UNIX_EPOCH.
+    /// A function C_i(t) estimating time at real time t.
     pub fn get_time(&self) -> i64{
 
-        let elapsed_real_secs = self.last_sync_system.elapsed().as_secs_f64();
+        let elapsed_real_secs = self.last_sync_system.elapsed().as_secs_f64(); // True deltaT, real time passed (not C_i(t))
 
         // Calculate the drift in microseconds: (seconds elapsed * drift rate)
         let drift_offset = (elapsed_real_secs * self.drift_rate) as i64;
-        let elapsed_micros = (elapsed_real_secs * 1_000_000.0) as i64;
+        let elapsed_real_micros = (elapsed_real_secs * 1_000_000.0) as i64;
 
         // Simulated time = Last Synced Time + Real Elapsed (in micros) + Drift
-        self.last_sync_simulated + elapsed_micros + drift_offset
+        self.last_sync_simulated + elapsed_real_micros + drift_offset
     }
 
+    /// Returns the uncertainty of the simulated clock.
+    /// Given as positive integer in microseconds.
     pub fn get_uncertainty(&self) -> i64{
         self.uncertainty
     }
 
 
-
+    /// Resynchronizes the simulated clock to current system time.
+    /// Resets the [`ClockSimulator::last_sync_simulated`] to time since UNIX_EPOCH in microseconds.
+    /// Resets the [`ClockSimulator::last_sync_system`] to Instant::now().
     pub fn sync_clock(&mut self){
         let now = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -73,7 +79,5 @@ impl ClockSimulator {
         self.last_sync_system = Instant::now();
 
     }
-
-
 }
 
